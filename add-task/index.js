8504c1232cd35961d50c05b753e44dd4cd1c2f70
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2020-10-07 09:26:47 -0500

    arrglos id y funcion get

--- add-task/index.js
+++ add-task/index.js
@@ -2,14 +2,14 @@
 module.exports = async function (context, req) {
     context.log('Adding a task to the planner');
     const task = req.body;
-    const id = uniqid(task.id);
+    const { v4: uuidv4 } = require('uuid');
+    const id= uuidv4();
     const descripcion = task.descripcion;
     const dueDate = task.dueDate;
     const status = task.status;
     const responsable = task.responsable;
   
-    const responseMessage = {id:id,descripcion:descripcion,
-        dueDate:dueDate,status:status,responsable:responsable};
+    const responseMessage = {id:id,descripcion:descripcion,dueDate:dueDate,status:status,responsable:responsable};
 
     context.res = {
         status: 201,
--- list-task/index.js
+++ list-task/index.js
@@ -8,6 +8,20 @@ module.exports = async function (context, req) {
             dueDate:new Date(),
             status: "Ready",
             responsable:{name:"admin",email:"admin@mail.com"}
+        },
+        {
+            id:uuidv4(),
+            descripcion:"prueba2",
+            dueDate:new Date(),
+            status: "Progress",
+            responsable:{name:"carlos",email:"carlos@mail.com"}
+        },
+        {
+            id:uuidv4(),
+            descripcion:"prueba3",
+            dueDate:new Date(),
+            status: "Completed",
+            responsable:{name:"andres",email:"andres@mail.com"}
         }
     ];                 
 
