3026a49b8dfb91c71910e602f7713f874edb26cb
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2020-10-06 22:37:30 -0500

    arreglos id

--- add-task/index.js
+++ add-task/index.js
@@ -2,14 +2,13 @@
 module.exports = async function (context, req) {
     context.log('Adding a task to the planner');
     const task = req.body;
-    const { v4: uuidv4 } = require('uuid');
-    const id = uuidv4();
+    const id = uniqid(task.id);
     const descripcion = task.descripcion;
     const dueDate = task.dueDate;
     const status = task.status;
     const responsable = task.responsable;
   
-    const responseMessage = {descripcion:descripcion,
+    const responseMessage = {id:id,descripcion:descripcion,
         dueDate:dueDate,status:status,responsable:responsable};
 
     context.res = {
